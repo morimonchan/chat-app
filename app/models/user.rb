cdb0ab6b8d14a3c4b2cd256a84c716001b827bdb
morimonchan <morimorimonchan.com@gmail.com>
2022-08-26 10:25:56 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ validates :name, presence: true
+#  もはや定型文。空の場合はDBに保存しないというバリデーション
+
 end
