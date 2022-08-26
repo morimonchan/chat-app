2b24f2f536f6e0bf346d5a205bcdb97f5fe4abee
morimonchan <morimorimonchan.com@gmail.com>
2022-08-26 12:46:11 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "rooms#index"
+  # rootをroomのindexへ指定。
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "messages#index"
+  # root to: "messages#index"
+  # 上記はトップページである、root indexへ移行するよう修正したため削除
   resources :users, only: [:edit, :update]
   # これによって/usersのパスに対応するルーティングが生成される、なお、onlyによって生成するルーティングを制限している
   # editは編集、updateは更新を表している。
