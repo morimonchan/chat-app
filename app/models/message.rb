ab1ceb1ef0a226c5e89864d624ca60a4ede782c4
morimonchan <morimorimonchan.com@gmail.com>
2022-08-27 10:04:01 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,11 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+  # メソッドの返り値がfalseならバリデーションによる検証を行うという意味
 
+  def was_attached?
+    self.image.attached?
+    # self.image.attached?で画像があればtrue、画像がなければfalseが返る。
+  end
 end
